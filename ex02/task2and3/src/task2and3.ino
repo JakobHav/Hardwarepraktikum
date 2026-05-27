b753491b16e59cbb7c8d6ee10e064a0657928c56
Jakob <jakob.hav26@gmail.com>
2026-05-27 14:42:27 +0200

    t2

--- ex02/task2and3/src/task2and3.ino
+++ ex02/task2and3/src/task2and3.ino
@@ -7,6 +7,42 @@
 //      Make necessary changes for setBuzzerFreq()
 // ------------------------------------------------------------
 
+#define GPIO 0x50000000
+#define OUTSET (GPIO + 0x508UL)
+#define IN (GPIO + 0x510UL)
+#define OUTCLR (GPIO + 0x50CUL)
+#define DIRSET (GPIO + 0x518UL)
+#define DIRCLR (GPIO + 0x51CUL)
+
+void setup() { ourPinMode(26, HIGH); }
+
+void loop() {
+  setP026(true);
+  delay(1000);
+  setP026(false);
+  delay(1000);
+}
+
+void ourPinMode(unsigned long pin, bool output) {
+  if (output) {
+    *(unsigned long *)DIRSET = (1UL << pin);
+  } else {
+    *(unsigned long *)DIRCLR = (1UL << pin);
+  }
+}
+
+void ourDigitalWrite(unsigned long pin, bool high) {
+  if (high) {
+    *(unsigned long *)OUTSET = (1UL << pin);
+  } else {
+    *(unsigned long *)OUTCLR = (1UL << pin);
+  }
+}
+
+bool ourDigitalRead(unsigned long pin) {
+  return (*(unsigned long*) IN) & (1UL << pin);
+  }
+
 #include <Arduino.h>
 
 
@@ -20,11 +56,9 @@ void loop() {
 }
 
 
-/*
 void setTimer1Freq() {
 
 }
-*/
 
 
 void setBuzzerFreq() {
