4f2f0b6850e0da3a56914b2b8e2995cece36a751
Sebastian Duttlinger <duttlinger@frohnat.de>
2026-05-27 14:58:57 +0200

    buzzer test

--- ex02/task2and3/src/task2and3.ino
+++ ex02/task2and3/src/task2and3.ino
@@ -7,6 +7,8 @@
 //      Make necessary changes for setBuzzerFreq()
 // ------------------------------------------------------------
 
+#include "delay.h"
+#include "wiring_digital.h"
 #define GPIO 0x50000000
 #define OUTSET (GPIO + 0x508UL)
 #define IN (GPIO + 0x510UL)
@@ -14,14 +16,7 @@
 #define DIRSET (GPIO + 0x518UL)
 #define DIRCLR (GPIO + 0x51CUL)
 
-void setup() { ourPinMode(26, HIGH); }
-
-void loop() {
-  setP026(true);
-  delay(1000);
-  setP026(false);
-  delay(1000);
-}
+#include <Arduino.h>
 
 void ourPinMode(unsigned long pin, bool output) {
   if (output) {
@@ -43,16 +38,22 @@ bool ourDigitalRead(unsigned long pin) {
   return (*(unsigned long*) IN) & (1UL << pin);
   }
 
-#include <Arduino.h>
-
 
 void setup() {
-  setTimer1Freq();
 }
 
+void playTone(int tone, int duration) {
+    for (long i = 0; i < duration * 1000L; i+= tone * 2) {
+        digitalWrite(A3, HIGH);
+        delayMicroseconds(tone);
+        digitalWrite(D3, LOW);
+        delayMicroseconds(tone);
+    }
+}
 
 void loop() {
-
+    delay(1000);
+    playTone(1014, 10000);
 }
 
 
@@ -71,4 +72,56 @@ extern "C" void TIMER1_IRQHandler() {
 }
 
 
+// -------------------------------------------------
 
+
+int speakerPin = D3;
+int length = 28; // the number of notes
+char notes[] = "GGAGcB GGAGdc GGxecBA yyecdc";
+int beats[] = { 2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 16 };
+int tempo = 150;
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = {'C', 'D', 'E', 'F', 'G', 'A', 'B',
+                  'c', 'd', 'e', 'f', 'g', 'a', 'b',
+                  'x', 'y'
+                 };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014,
+                  956,  834,  765,  593,  468,  346,  224,
+                  655 , 715
+                };
+  int SPEE = 5;
+
+  // play the tone corresponding to the note name
+
+  for (int i = 0; i < 16; i++) {
+    if (names[i] == note) {
+      int newduration = duration / SPEE;
+      playTone(tones[i], newduration);
+    }
+  }
+}
+
+// void setup() {
+//   pinMode(speakerPin, OUTPUT);
+// }
+
+// void loop() {
+//   for (int i = 0; i < length; i++) {
+//     if (notes[i] == ' ') {
+//       delay(beats[i] * tempo); // rest
+//     } else {
+//       playNote(notes[i], beats[i] * tempo);
+//     }
+//     // pause between notes
+//     delay(tempo);
+//   }
+// }
