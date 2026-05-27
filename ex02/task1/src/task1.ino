4f9a0b36f2f12e7ac8d3332278e0d53da8bfae64
Jakob <jakob.hav26@gmail.com>
2026-05-27 14:18:09 +0200

    task1

--- ex02/task1/src/task1.ino
+++ ex02/task1/src/task1.ino
@@ -1,5 +1,30 @@
-void setup(){}
+#define GPIO 0x50000000
+#define OUTSET (GPIO + 0x508UL)
+#define OUTCLR (GPIO + 0x50CUL)
+#define DIRSET (GPIO + 0x518UL)
+#define DIRCLR (GPIO + 0x51CUL)
 
-void loop(){}
+void setup() { ourPinMode(26, HIGH); }
 
-void setP026(boolean high) {}
+void loop() {
+  setP026(true);
+  delay(1000);
+  setP026(false);
+  delay(1000);
+}
+
+void ourPinMode(unsigned long pin, bool high) {
+  if (high) {
+    *(unsigned long *)DIRSET = (1 << pin);
+  } else {
+    *(unsigned long *)DIRCLR = (1 << pin);
+  }
+}
+
+void setP026(bool high) {
+  if (high) {
+    *(unsigned long *)OUTSET = (1 << 26);
+  } else {
+    *(unsigned long *)OUTCLR = (1 << 26);
+  }
+}
