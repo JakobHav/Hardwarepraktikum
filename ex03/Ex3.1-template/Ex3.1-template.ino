6f5e429b9a025e1ed7d7b8edefde2f424bcc85c7
Jakob <jakob.hav26@gmail.com>
2026-06-12 14:57:36 +0200

    task1

--- ex03/Ex3.1-template/Ex3.1-template.ino
+++ ex03/Ex3.1-template/Ex3.1-template.ino
@@ -1,31 +1,66 @@
 // Task 1: Environmental Light Categorization
 // Complete the implementation according to the task description.
 
-// TODO: define necessary constants (e.g., calibration range, thresholds, timing)
+// Defining necessary constants (e.g., calibration range, thresholds,
+// timing)
 
-// TODO: implement a function that categorizes the normalized value
-const char* categorize(int normalized) {
-    // TODO: return "LOW", "MEDIUM", or "HIGH"
+#define dark 50UL
+#define bright 3500UL
+#define depth_12bit 2
+
+#define SAADC 0x40007000
+#define RESOLUTION_OFFSET 0x5F0
+
+#define lowThresh 33
+#define highThresh 66
+
+// Categorizing the normalized value
+const char *categorize(int normalized) {
+  if (normalized <= lowThresh) {
+
+    return "LOW";
+  } else if (normalized < highThresh) {
+
+    return "MEDIUM";
+  } else {
+
+    return "HIGH";
+  }
 }
 
 void setup() {
-    // TODO: initialize Serial communication
+  // Initializing Serial communication
+  Serial.begin(115200);
 
-    // TODO: configure ADC resolution to 12-bit
+  // Configureing ADC resolution to 12-bit
 
+  (char *)(SAADC + RESOLUTION_OFFSET)->depth_12bit;
 }
 
 void loop() {
-    // TODO: implement non-blocking timing using millis()
-    // The system should sample every 500 ms
+  // Non-blockingly sampleing every 500 ms
+  unsigned long start = millis();
+  if (millis() - start >= 500) {
 
-    // TODO: read raw value from light sensor
+    // Reading raw value from light sensor
+    unsigned long value = analogRead(1);
 
-    // TODO: clamp the raw value to a calibrated range
+    // Clamping the raw value to a calibrated range
+    clamp(value, dark, bright);
 
-    // TODO: normalize the value to a 0–100 scale
+    // Normalizing the value to a 0–100 scale
+    unsigned long normalized = map(value, dark, bright, 0, 100);
 
-    // TODO: determine the category using the categorize() function
+    // Determining the category using the categorize() function
+    const char *cat = categorize(normalized);
 
-    // TODO: print raw value, normalized value, and category to Serial
+    // Printing raw value, normalized value, and category to Serial
+
+    Serial.print("R=");
+    Serial.print(raw);
+    Serial.print(", N=");
+    Serial.print(normalized);
+    Serial.print(", C=");
+    Serial.println(cat);
+  }
 }
